df68da14a8475a4c07b167900ee825eb8244e189
hakureii <hakurei@asia.com>
2024-08-07 06:55:27 +0530

    bleh

---
+++ ai.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+from groq import Groq
+
+class Ai:
+    def __init__(self):
+        self.model = "llama-3.1-70b-versatile"
+        self.model = "llama3-70b-8192"
+        self.client = Groq(api_key=os.getenv("GROQ"))
+        with open("system.json") as file:
+            import json
+            self.history = [json.load(file)]
+
+    def response(self, message: str = ""):
+        self.update_history(role="user",message=message)
+        ai_response = self.client.chat.completions.create(
+                model = self.model,
+                max_tokens = 512,
+                messages=self.history
+        )
+        self.update_history(message=ai_response.choices[0].message.content)
+        return ai_response.choices[0].message.content
+
+    def update_history(self, role: str = "assistant", message: str = ""):
+        self.history.append(
+            {
+                "role": role,
+                "content": message
+            }
+        )
+
+ai = Ai()
+
+def main():
+    os.system("clear")
+    print("Welcome to the AI Chat! Type 'q' to quit.")
+    while True:
+        print("_" * shutil.get_terminal_size().columns)
+        prompt = input("\nYour Input: ")
+
+        if prompt.lower() == "q":
+            print("Goodbye!")
+            break
+
+        response = ai.response(prompt)
+        print(f"\n\033[0;34m{response}\033[0m\n")
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,77 +1,21 @@
 import os
-import json
-import asyncio
 import discord
-from groq import Groq
+from ai import Ai
 from discord.ext import commands
 
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(command_prefix=[], intents=intents)
 
-intents = discord.Intents.all()
-bot = commands.Bot(["* "], intents=intents)
-client = Groq(api_key=os.getenv("GROQ"))
-model = "llama-3.1-70b-versatile"
-
-with open("./ai_history.json") as file:
-    default_history = json.load(file)
-
-def update_history(message:str, history:list, role: str = "user"):
-    history.append(
-        {
-            "role": role,
-            "content": message
-        }
-    )
-    return history
-
-def fix_discord_message(message: str):
-    # fix the discord embed issues.
-    special_syms = ["*", "_", "-", "~", "#"]
-    fixed_message = ""
-    for char in message:
-        if char in special_syms:
-            fixed_message += "\\" + char
-        else:
-            fixed_message += char
-    # fix overlapping size for discord limit
-    return fixed_message.split(".\n")
-
-global ai_mode, ai_channels
-ai_channels = {}
-
-@bot.command(name="ai")
-async def ai(ctx: commands.Context):
-    chan_id = ctx.channel.id
-    if len(ai_channels) >= 1:
-        if chan_id in ai_channels:
-            ai_channels.pop(chan_id)
-            await ctx.reply("disabled!")
-            return
-    ai_channels[chan_id] = default_history
-    await ctx.reply("enabled!")
+bot = Bot()
+ai = Ai()
 
 @bot.event
 async def on_message(message: discord.Message):
-    if message.author.bot or message.author == bot.user:
+    if message.author.bot:
         return
-    if message.content.startswith("* "):
-        await bot.process_commands(message)
-        return
-    if message.channel.id in ai_channels:
-        await message.channel.trigger_typing()
-        response = client.chat.completions.create(model=model,messages=update_history(message.content, ai_channels[message.channel.id]), max_tokens=512)
-        update_history(response.choices[0].message.content, ai_channels[message.channel.id], "assistant")
-        for chunk in fix_discord_message(response.choices[0].message.content):
-            await message.channel.send(content=chunk)
-
-@bot.command(name="update")
-async def update(ctx: commands.Context):
-    await ctx.send("-# Soon™")
-    await bot.close()
-
-@bot.event
-async def on_ready():
-    await asyncio.sleep(5 * 60 * 60)
-    os.remove(os.environ["CONDITION"])
-    await bot.close()
+    if message.channel == bot.get_channel(1270529190533271582):
+        await message.reply(ai.response(message.content))
 
 bot.run(os.getenv("TOKEN"))
